a622e2c93659a47848a485c2a80470b1eca3e634
Guilherme Luis <guile.hm@hotmail.com>
2019-01-27 21:35:35 -0200

    Improve bot

--- bot/discord.py
+++ bot/discord.py
@@ -1,7 +1,10 @@
+import asyncio
 import json
 import os
 import sys
+from itertools import cycle
 
+import discord
 import requests
 from chatterbot import ChatBot
 from chatterbot.trainers import ListTrainer
@@ -32,10 +35,29 @@ messages = json.loads(message_file.content)
 trainer = ListTrainer(bot)
 trainer.train(messages)
 
+games_list = ['Tibia', 'Fortnite', 'CS GO', 'Resident Evil 7', 'Fifa 2019', 'Divinity: Original Sin 2', 'PUBG']
 
 @client.event
 async def on_ready():
     print('Bot está pronto :)')
+
+
+@client.event
+async def on_member_join(member):
+    server = member.server
+    default_channel = server.default_channel
+    await client.send_message(default_channel, 'Olá {}, seja bem-vindo!'.format(member.mention))
+
+
+@client.event
+async def change_status():
+    await client.wait_until_ready()
+    msgs = cycle(games_list)
+
+    while not client.is_closed:
+        current_status = next(msgs)
+        await client.change_presence(game=discord.Game(name=current_status))
+        await asyncio.sleep(120)
 
 
 @client.event
@@ -59,4 +81,5 @@ async def on_message(message):
 
 
 if __name__ == '__main__':
+    client.loop.create_task(change_status())
     client.run(DISCORD_BOT_TOKEN)
