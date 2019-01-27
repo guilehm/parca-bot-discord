c7423a617d2ac223277e73f94cb7a43518f5d305
Guilherme Luis <guile.hm@hotmail.com>
2019-01-27 14:54:04 -0200

    Update settings

--- parcaBot/settings.py
+++ parcaBot/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'chatterbot.ext.django_chatterbot',
+    'bot',
 ]
 
 MIDDLEWARE = [
